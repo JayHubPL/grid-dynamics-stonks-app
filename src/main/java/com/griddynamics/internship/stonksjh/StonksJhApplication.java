d914bc61576bb7c805a80561afc765096b87b7c0
Jakub Łaba <jlaba@griddynamics.com>
2022-12-05 14:19:10 +0100

    Add initial directory structure and adjust project name

---
+++ src/main/java/com/griddynamics/internship/stonksjh/StonksJhApplication.java
@@ -0,0 +1,13 @@
+package com.griddynamics.internship.stonksjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonksJhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonksJhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/griddynamics/internship/stonksjh/StonksJhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamics.internship.stonksjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonksJhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
