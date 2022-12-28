12079dd48222a1b48b911cb20bccbcc43eded560
Hubert Mazur <hmazur@griddynamics.com>
2022-12-28 17:43:21 +0100

    Add all args constructor

--- src/main/java/com/griddynamics/internship/stonksjh/order/dto/OrderDTO.java
+++ src/main/java/com/griddynamics/internship/stonksjh/order/dto/OrderDTO.java
@@ -6,11 +6,15 @@ import org.springframework.hateoas.RepresentationModel;
 
 import com.griddynamics.internship.stonksjh.order.model.OrderType;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDTO extends RepresentationModel<OrderDTO>{
     
     private UUID uuid;
