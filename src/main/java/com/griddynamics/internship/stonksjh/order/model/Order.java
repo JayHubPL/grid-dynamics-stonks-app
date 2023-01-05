82ac572c61f552d2fd264a79d5076710b41894c3
Hubert Mazur <hmazur@griddynamics.com>
2023-01-05 12:48:00 +0100

    Remove unnecesary names of tables

--- src/main/java/com/griddynamics/internship/stonksjh/order/model/Order.java
+++ src/main/java/com/griddynamics/internship/stonksjh/order/model/Order.java
@@ -5,7 +5,6 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -14,7 +13,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "Order")
 @Table(name = "order")
 @NoArgsConstructor
 @Getter
@@ -26,14 +24,12 @@ public class Order {
         strategy = IDENTITY
     )
     @Column(
-        name = "id",
         updatable = false
     )
     @Setter(AccessLevel.NONE)
     private long id;
 
     @Column(
-        name = "uuid",
         updatable = false,
         nullable = false,
         unique = true
@@ -41,19 +37,16 @@ public class Order {
     private UUID uuid;
 
     @Column(
-        name = "orderType",
         nullable = false
     )
     private OrderType orderType;
 
     @Column(
-        name = "amount",
         nullable = false
     )
     private int amount;
 
     @Column(
-        name = "symbol",
         nullable = false
     )
     private String symbol;
