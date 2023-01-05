6dfcf61f43a97f98da9b59b59649983011ccc8ee
Hubert Mazur <hmazur@griddynamics.com>
2023-01-05 12:47:39 +0100

    Remove TODO from OrderMapper

--- src/main/java/com/griddynamics/internship/stonksjh/order/mapper/OrderMapper.java
+++ src/main/java/com/griddynamics/internship/stonksjh/order/mapper/OrderMapper.java
@@ -2,19 +2,18 @@ package com.griddynamics.internship.stonksjh.order.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
 
 import com.griddynamics.internship.stonksjh.order.dto.OrderDTO;
 import com.griddynamics.internship.stonksjh.order.model.Order;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
 
     @Mapping(target = "uuid", source = "uuid")
     @Mapping(target = "orderType", source = "orderType")
     @Mapping(target = "amount", source = "amount")
     @Mapping(target = "symbol", source = "symbol")
-    OrderDTO entityToDto(Order entity); // TODO Unmapped target property: "add" (what is this?)
+    OrderDTO entityToDto(Order entity);
 
     @Mapping(target = "uuid", source = "uuid")
     @Mapping(target = "orderType", source = "orderType")
