1287145b990d7139a0a07b1400c14817657eef52
Hubert Mazur <hmazur@griddynamics.com>
2023-01-11 14:01:05 +0100

    Remove unnecessary annotations from OrderMapper

--- src/main/java/com/griddynamics/internship/stonksjh/order/mapper/OrderMapper.java
+++ src/main/java/com/griddynamics/internship/stonksjh/order/mapper/OrderMapper.java
@@ -1,7 +1,6 @@
 package com.griddynamics.internship.stonksjh.order.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import com.griddynamics.internship.stonksjh.order.dto.OrderDTO;
 import com.griddynamics.internship.stonksjh.order.model.Order;
@@ -9,16 +8,8 @@ import com.griddynamics.internship.stonksjh.order.model.Order;
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
 
-    @Mapping(target = "uuid", source = "uuid")
-    @Mapping(target = "orderType", source = "orderType")
-    @Mapping(target = "amount", source = "amount")
-    @Mapping(target = "symbol", source = "symbol")
     OrderDTO entityToDto(Order entity);
 
-    @Mapping(target = "uuid", source = "uuid")
-    @Mapping(target = "orderType", source = "orderType")
-    @Mapping(target = "amount", source = "amount")
-    @Mapping(target = "symbol", source = "symbol")
     Order dtoToEntity(OrderDTO dto);
 
 }
