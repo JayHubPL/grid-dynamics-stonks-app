04a02632158261e30f723debab086dd06c14d4c5
Hubert Mazur <hmazur@griddynamics.com>
2023-01-18 17:02:54 +0100

    Remove unused methods from TestDataFactory

--- src/test/java/com/griddynamics/internship/stonksjh/util/TestDataFactory.java
+++ src/test/java/com/griddynamics/internship/stonksjh/util/TestDataFactory.java
@@ -1,12 +1,8 @@
 package com.griddynamics.internship.stonksjh.util;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.params.provider.Arguments;
-
-import com.griddynamics.internship.stonksjh.order.model.OrderType;
-import com.griddynamics.internship.stonksjh.order.model.Symbol;
 
 public class TestDataFactory {
     
@@ -24,16 +20,6 @@ public class TestDataFactory {
             Arguments.of(1, "GOOG", "*"),
             Arguments.of(-5, "", "")
         );
-    }
-
-    public static Stream<Arguments> validStringStockSymbols() {
-        return Arrays.stream(Symbol.values())
-            .map(symbol -> Arguments.of(symbol.toString()));
-    }
-
-    public static Stream<Arguments> validStringOrderTypes() {
-        return Arrays.stream(OrderType.values())
-            .map(orderType -> Arguments.of(orderType.toString()));
     }
 
 }
