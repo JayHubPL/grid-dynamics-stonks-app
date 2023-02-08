3465d64e46dd5e9ee09db77718487e6737e3904d
Jakub Łaba <jlaba@griddynamics.com>
2023-02-08 14:46:57 +0100

    Refactor BrokerService

--- src/main/java/com/griddynamics/internship/stonksjh/service/BrokerService.java
+++ src/main/java/com/griddynamics/internship/stonksjh/service/BrokerService.java
@@ -3,11 +3,12 @@ package com.griddynamics.internship.stonksjh.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.griddynamics.internship.stonksjh.exception.trading.InsufficientBalanceException;
-import com.griddynamics.internship.stonksjh.exception.trading.InsufficientStockAmountException;
+import com.griddynamics.internship.stonksjh.exception.broker.InsufficientBalanceException;
+import com.griddynamics.internship.stonksjh.exception.broker.InsufficientStockAmountException;
 import com.griddynamics.internship.stonksjh.model.Order;
 import com.griddynamics.internship.stonksjh.model.User;
 import com.griddynamics.internship.stonksjh.properties.FinnhubProperties;
+import com.griddynamics.internship.stonksjh.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.http.HttpStatus;
@@ -20,11 +21,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
@@ -32,47 +32,35 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.griddynamics.internship.stonksjh.exception.broker.InsufficientBalanceException;
-import com.griddynamics.internship.stonksjh.exception.broker.InsufficientStockAmountException;
-import com.griddynamics.internship.stonksjh.exception.broker.MissingFinnhubApiTokenException;
-import com.griddynamics.internship.stonksjh.model.Order;
-import com.griddynamics.internship.stonksjh.model.User;
-import com.griddynamics.internship.stonksjh.repository.OrderRepository;
-
 @Service
 @Slf4j
 public class BrokerService {
 
-    private final String FINNHUB_TOKEN_HEADER = "X-Finnhub-Token";
-    private final String FINNHUB_API_QUOTE_ENDPOINT = "https://finnhub.io/api/v1/quote?symbol=%s";
-    private final Path FINNHUB_TOKEN_PATH = Path.of("token.txt");
-    private final BigDecimal COMMISSION_PERCENT = new BigDecimal("0.07"); // 7% commission
-    private final HttpClient httpClient = HttpClient.newHttpClient();
+    private static final String ENDPOINT_SUFFIX = "?symbol=%s";
+    private static final BigDecimal COMMISSION_PERCENT = new BigDecimal("0.07"); // 7% commission
+    private final HttpClient httpClient;
     private final ObjectMapper objectMapper;
-    private final String token;
-
+    private final OrderRepository orderRepository;
+    private final FinnhubProperties finnhubProperties;
     private Map<Order.Symbol, BigDecimal> stockPrices;
 
-    private final OrderRepository orderRepository;
-
-    public BrokerService(OrderRepository orderRepository, ObjectMapper objectMapper) {
-        this.orderRepository = orderRepository;
+    public BrokerService(
+            HttpClient httpClient,
+            ObjectMapper objectMapper,
+            OrderRepository orderRepository,
+            FinnhubProperties finnhubProperties
+    ) {
+        this.httpClient = httpClient;
         this.objectMapper = objectMapper;
-        try {
-            token = Files.readString(FINNHUB_TOKEN_PATH);
-        } catch (IOException e) {
-            throw new MissingFinnhubApiTokenException();
-        }
+        this.orderRepository = orderRepository;
+        this.finnhubProperties = finnhubProperties;
         initializeDaemonThread(1);
     }
 
     public void checkIfOrderCanBePlaced(Order order) {
         User owner = order.getOwner();
+        val stocks = owner.getStocks();
+        val stockCount = stocks.get(order.getSymbol());
         switch (order.getType()) {
             case BUY -> {
                 BigDecimal actualBalance = owner.getBalance().subtract(getPendingExpenditures(owner));
@@ -82,13 +70,9 @@ public class BrokerService {
                 }
             }
             case SELL -> {
-                long stockCount = orderRepository.findAllByOwnerUuidAndSymbol(owner.getUuid(), order.getSymbol()).stream()
-                        .mapToLong(o -> (long)o.getAmount() * (o.getType() == Order.Type.BUY ? 1 : -1))
-                        .sum();
                 if (order.getAmount() > stockCount) {
                     throw new InsufficientStockAmountException(order.getAmount(), order.getSymbol(), stockCount);
                 }
-                stocks.computeIfPresent(order.getSymbol(), (k, v) -> v - order.getAmount());
             }
         }
     }
@@ -117,9 +101,9 @@ public class BrokerService {
                 .filter(order -> order.getStatus() == Order.Status.PENDING)
                 .filter(order -> order.getType() == Order.Type.BUY)
                 .reduce(
-                    BigDecimal.ZERO,
-                    (acc, order) -> acc.add(order.getBid().multiply(BigDecimal.valueOf(order.getAmount()))),
-                    BigDecimal::add
+                        BigDecimal.ZERO,
+                        (acc, order) -> acc.add(order.getBid().multiply(BigDecimal.valueOf(order.getAmount()))),
+                        BigDecimal::add
                 );
         return applyCommission(pendingExpenditures);
     }
@@ -127,18 +111,21 @@ public class BrokerService {
     private void processOrder(Order order) {
         User owner = order.getOwner();
         BigDecimal orderValue = evaluateOrderValue(order);
+        val stocks = owner.getStocks();
         switch (order.getType()) {
             case BUY -> {
                 BigDecimal actualBalance = owner.getBalance().subtract(getPendingExpenditures(owner));
                 if (applyCommission(order.getBid()).compareTo(stockPrices.get(order.getSymbol())) < 0 || actualBalance.compareTo(orderValue) < 0) {
                     return; // can't be processed yet
                 }
+                stocks.merge(order.getSymbol(), order.getAmount(), Integer::sum);
                 owner.setBalance(owner.getBalance().subtract(orderValue));
             }
             case SELL -> {
                 if (order.getBid().compareTo(stockPrices.get(order.getSymbol())) < 0) {
                     return; // can't be processed yet
                 }
+                stocks.computeIfPresent(order.getSymbol(), (k, v) -> v - order.getAmount());
                 owner.setBalance(owner.getBalance().add(orderValue.multiply(BigDecimal.ONE.subtract(COMMISSION_PERCENT))));
             }
         }
@@ -150,7 +137,7 @@ public class BrokerService {
         return applyCommission(order.getBid().multiply(BigDecimal.valueOf(order.getAmount())));
     }
 
-    private BigDecimal getStockPrice(Order.Symbol symbol) {
+    private Optional<BigDecimal> getStockPrice(Order.Symbol symbol) {
         val endpoint = finnhubProperties.apiUrl() + ENDPOINT_SUFFIX;
         try {
             URI uri = URI.create(String.format(endpoint, symbol.toString()));
@@ -164,27 +151,28 @@ public class BrokerService {
                 throw new RuntimeException("Got response with the status code " + HttpStatus.valueOf(response.statusCode()));
             }
 
-            return new BigDecimal(objectMapper.readValue(response.body(), ObjectNode.class).get("c").asText());
+            return Optional.of(
+                    new BigDecimal(objectMapper.readValue(response.body(), ObjectNode.class).get("c").asText())
+            );
         } catch (JsonProcessingException e) {
-            val msg = "Failed to parse response from finnhub.io";
-            log.error(msg);
-            throw new RuntimeException(msg, e);
+            log.error("Failed to parse response from finnhub.io");
+            return Optional.empty();
         } catch (IOException | InterruptedException e) {
-            val msg = "Failed to send a request to finnhub.io";
-            log.error(msg);
-            throw new RuntimeException(msg, e);
+            log.error("Failed to send a request to finnhub.io");
+            return Optional.empty();
         }
     }
 
     private Map<Order.Symbol, BigDecimal> getAllStockPrices() {
         return Arrays.stream(Order.Symbol.values())
                 .collect(Collectors.toMap(
-                    Function.identity(),
-                    symbol -> getStockPrice(symbol),
-                    (o1, o2) -> {
-                        throw new IllegalArgumentException("Key collision"); // this should technically never happen
-                    }, 
-                    () -> new EnumMap<>(Order.Symbol.class)
+                        Function.identity(),
+                        symbol -> getStockPrice(symbol)
+                                .orElse(stockPrices.get(symbol)),
+                        (o1, o2) -> {
+                            throw new IllegalArgumentException("Key collision"); // this should technically never happen
+                        },
+                        () -> new EnumMap<>(Order.Symbol.class)
                 ));
     }
 
